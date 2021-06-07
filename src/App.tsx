f0c6f13c24a9a2dd37e0db6cccf81b04b5972d9b
Hassan Raza <hassan.raza@shipsy.co.in>
2021-06-07 19:11:06 +0530

    testing cd 2

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
     <div className="App">
       <h1>Welcome To LinkedList Creator!!</h1>
       <br />
-      <h2>Testing multi url deploy 1</h2>
+      <h2>Testing multi url deploy 2</h2>
       <input type="text" value={data} onChange={handleData} />
       <button onClick={addData}>Add</button>
       <br />
